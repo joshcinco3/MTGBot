af75a2b991bf94489d12b51b1a6df3899a358df5
joshcinco3 <josh.cinco.3@gmail.com>
2021-09-15 15:28:18 -0700

    keyword generator extracted to separate file.

--- commands/thanos.js
+++ commands/thanos.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { thanosSwitch } = require('../thanosQuotes.js');
-const { thanosNumberPicker } = require('../thanosPicker');
+const { thanosPickerNum, thanosPickerStr } = require('../thanosPicker');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,87 +8,46 @@ module.exports = {
 		.setDescription('Replies with a Thanos quote.')
 		.addIntegerOption((option) =>
 			option
-				.setName('quotenum')
+				.setName('tnum')
 				.setDescription('Allows a user to choose a specific Thanos quote based on number (Leave blank for random quote).')
 				.setRequired(false),
 		)
 		.addStringOption((option) =>
 			option
-				.setName('thanoskeyword')
+				.setName('tkeyword')
 				.setDescription('Allows a user to choose a specific Thanos quote based on keyword (Leave blank for random quote).')
 				.setRequired(false),
 		),
 	async execute(interaction) {
-		let thanosQuoteIndex = 0;
-		let userQuoteNum = 0;
+		let tQuoteIndex = 0;
 
-		if (!interaction.options.get('quotenum') && !interaction.options.get('thanoskeyword')) {
+		if (!interaction.options.get('tnum') && !interaction.options.get('tkeyword')) {
 			const min = Math.ceil(1);
 			const max = Math.floor(22);
-			thanosQuoteIndex = Math.floor(Math.random() * (max - min + 1) + min);
+			tQuoteIndex = Math.floor(Math.random() * (max - min + 1) + min);
 		}
-		else if (interaction.options.get('quotenum') && !interaction.options.get('thanoskeyword')) {
-			userQuoteNum = interaction.options.get('quotenum').value;
+		else if (interaction.options.get('tnum') && !interaction.options.get('tkeyword')) {
+			const userQuoteNum = interaction.options.get('tnum').value;
 
-			thanosQuoteIndex = thanosNumberPicker(userQuoteNum);
+			tQuoteIndex = thanosPickerNum(userQuoteNum);
 
-			if (isNaN(thanosQuoteIndex)) {
-				return interaction.reply('Reality has disappointed us once again as it appears you have entered an invalid number. Try once more little one.');
+			if (isNaN(tQuoteIndex)) {
+				return interaction.reply('Reality has disappointed us once again as it appears you have entered an invalid number. Try once more, little one.');
 			}
 		}
-		else if (!interaction.options.get('quotenum') && interaction.options.get('thanoskeyword')) {
-			const userKeyword = interaction.options.get('thanoskeyword').value.toLowerCase();
+		else if (!interaction.options.get('tnum') && interaction.options.get('tkeyword')) {
+			const userKeyword = interaction.options.get('tkeyword').value.toLowerCase();
 
-			if (userKeyword === 'myself') {
-				thanosQuoteIndex = 1;
-			}
-			else if (userKeyword === 'dread' || userKeyword === 'run' || userKeyword === 'destiny') {
-				thanosQuoteIndex = 2;
-			}
-			else if (userKeyword === 'balanced') {
-				thanosQuoteIndex = 4;
-			}
-			else if (userKeyword === 'reality') {
-				thanosQuoteIndex = 5;
-			}
-			else if (userKeyword === 'price' || userKeyword === 'salvation') {
-				thanosQuoteIndex = 7;
-			}
-			else if (userKeyword.replace(/\s+/g, '') === 'litleone') {
-				thanosQuoteIndex = 9;
-			}
-			else if (userKeyword === 'choices' || userKeyword === 'wills') {
-				thanosQuoteIndex = 12;
-			}
-			else if (userKeyword === 'cursed' || userKeyword === 'knowledge') {
-				thanosQuoteIndex = 13;
-			}
-			else if (userKeyword === 'head') {
-				thanosQuoteIndex = 15;
-			}
-			else if (userKeyword === 'atoms') {
-				thanosQuoteIndex = 16;
-			}
-			else if (userKeyword === 'inevitable') {
-				thanosQuoteIndex = 17;
-			}
-			else if (userKeyword === 'harshly') {
-				thanosQuoteIndex = 18;
-			}
-			else if (userKeyword === 'failure') {
-				thanosQuoteIndex = 19;
-			}
-			else if (userKeyword === 'stones') {
-				thanosQuoteIndex = 22;
-			}
-			else {
-				return interaction.reply('Invalid keyword. Try once more, little one.');
+			tQuoteIndex = thanosPickerStr(userKeyword);
+
+			if (tQuoteIndex === -1) {
+				return interaction.reply('Reality has disappointed us once again as it appears you have entered an invalid keyword. Try once more, little one.');
 			}
 		}
 		else {
-			return interaction.reply('Reality is often disappointing. Looks like the quote generator failed this time, try once more little one.');
+			return interaction.reply('Reality is often disappointing. Looks like the quote generator failed this time. Try once more, little one.');
 		}
 
-		await interaction.reply(thanosSwitch(thanosQuoteIndex));
+		await interaction.reply(thanosSwitch(tQuoteIndex));
 	},
 };
--- thanosPicker.js
+++ thanosPicker.js
@@ -16,4 +16,52 @@ const thanosPickerNum = (userNum) => {
 	}
 };
 
-module.exports = { thanosPickerNum };
+const thanosPickerStr = (userStr) => {
+	if (userStr === 'myself') {
+		return 1;
+	}
+	else if (userStr === 'dread' || userStr === 'run' || userStr === 'destiny') {
+		return 2;
+	}
+	else if (userStr === 'balanced') {
+		return 4;
+	}
+	else if (userStr === 'reality') {
+		return 5;
+	}
+	else if (userStr === 'price' || userStr === 'salvation') {
+		return 7;
+	}
+	else if (userStr.replace(/\s+/g, '') === 'littleone') {
+		return 9;
+	}
+	else if (userStr === 'choices' || userStr === 'wills') {
+		return 12;
+	}
+	else if (userStr === 'cursed' || userStr === 'knowledge') {
+		return 13;
+	}
+	else if (userStr === 'head') {
+		return 15;
+	}
+	else if (userStr === 'atoms') {
+		return 16;
+	}
+	else if (userStr === 'inevitable') {
+		return 17;
+	}
+	else if (userStr === 'harshly') {
+		return 18;
+	}
+	else if (userStr === 'failure') {
+		return 19;
+	}
+	else if (userStr === 'stones') {
+		return 22;
+	}
+	else {
+		return -1;
+	}
+};
+
+module.exports = { thanosPickerNum, thanosPickerStr };
